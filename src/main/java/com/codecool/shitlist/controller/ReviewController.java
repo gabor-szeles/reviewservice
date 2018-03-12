bbf083a2a7a22d657c9f84b3957a8e58d592905a
gabor-szeles <szelesgbr@gmail.com>
2018-03-12 15:07:32 +0100

    added paths for get and put

--- src/main/java/com/codecool/shitlist/controller/ReviewController.java
+++ src/main/java/com/codecool/shitlist/controller/ReviewController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -21,5 +23,13 @@ public class ReviewController {
         Map<String, String> testMap = new HashMap<>();
         testMap.put("test", "test");
         return new ResponseEntity(testMap, HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/addreview")
+    public ResponseEntity addReview(@RequestParam("userId") Long userId,
+                                    @RequestParam("authorId") Long authorId,
+                                    @RequestParam("review") String reviewText) {
+        reviewService.addReview(userId, authorId, reviewText);
+        return new ResponseEntity("OK", HttpStatus.OK);
     }
 }
