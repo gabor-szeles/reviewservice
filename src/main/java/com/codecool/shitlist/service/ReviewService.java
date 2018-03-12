24aec074ae6f8fd5cbdc7e08a679ecedf4237c87
gabor-szeles <szelesgbr@gmail.com>
2018-03-12 15:07:50 +0100

    add review method added

--- src/main/java/com/codecool/shitlist/service/ReviewService.java
+++ src/main/java/com/codecool/shitlist/service/ReviewService.java
@@ -1,5 +1,6 @@
 package com.codecool.shitlist.service;
 
+import com.codecool.shitlist.model.Review;
 import com.codecool.shitlist.repository.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,9 @@ public class ReviewService {
 
     @Autowired
     private ReviewRepository reviewRepository;
+
+    public void addReview(Long userId, Long authorId, String reviewText) {
+        Review toAdd = new Review(authorId, reviewText, userId);
+        reviewRepository.save(toAdd);
+    }
 }
