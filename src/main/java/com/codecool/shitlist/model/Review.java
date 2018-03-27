cfa1855480080c8e30326f33cefa1769bcdd0681
Szűcs Tibor <job.szucstibor@gmail.com>
2018-03-27 12:43:17 +0200

    Added comment title and date fields to review class

--- src/main/java/com/codecool/shitlist/model/Review.java
+++ src/main/java/com/codecool/shitlist/model/Review.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
 
 @Entity
 public class Review {
@@ -14,20 +16,29 @@ public class Review {
 
     private Long authorId;
 
+    private String title;
+
+    private String comment;
+
     private String description;
 
     private Long userId;
 
     private Long rating;
 
+    private LocalDate date;
+
     public Review() {
     }
 
-    public Review(Long authorId, String description, Long userId, Long rating) {
+    public Review(Long authorId, String title, String description, Long userId, Long rating, String comment) {
         this.authorId = authorId;
+        this.title = title;
         this.description = description;
         this.userId = userId;
         this.rating = rating;
+        this.comment = comment;
+        this.date = ZonedDateTime.now().toLocalDate();
     }
 
     public Long getId() {
@@ -64,5 +75,29 @@ public class Review {
 
     public void setRating(Long rating) {
         this.rating = rating;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
     }
 }
